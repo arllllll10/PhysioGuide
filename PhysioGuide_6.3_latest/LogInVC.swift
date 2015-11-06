b273025a0293b8a0123353456856c0c47329aee5
Thomas Breen <tbreen@asb9700-e05.csil.sfu.ca>
2015-11-05 21:05:49 -0800

    Initial Update

---
+++ PhysioGuide_6.3_latest/LogInVC.swift
@@ -0,0 +1,25 @@
+//
+//  LogInVC.swift
+//  PhysioGuide_6.3_latest
+//
+//  Created by Thomas Breen on 2015-11-05.
+//  Copyright (c) 2015 Team STAR. All rights reserved.
+//
+
+import UIKit
+
+class LogInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
