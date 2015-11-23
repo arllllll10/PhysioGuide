139e6e653a44c99b627dec5270fa835ecb6b9877
Thomas Breen <tbreen@asb9700-e01.csil.sfu.ca>
2015-11-22 16:55:46 -0800

    minor changes to newRoutine

--- PhysioGuide_6.3_latest/CreateRoutineVC.swift
+++ PhysioGuide_6.3_latest/CreateRoutineVC.swift
@@ -12,8 +12,15 @@ import UIKit
 
 class CreateRoutineVC: UIViewController, UICollectionViewDelegate {
     
+    // MARK: Vars
+    
     @IBOutlet weak var ListContainer: UIView!
     @IBOutlet weak var CurrentRoutineContainer: UIView!
+    
+    @IBOutlet weak var nameYourRoutine: UILabel! // UILabel telling the user to enter name of routine
+    @IBOutlet weak var routineName: UITextField! // UITextField for user to enter name into
+    @IBOutlet weak var createButton: UIButton! // Button for user to click when they have entered name
+    @IBOutlet weak var doneSelectingExercisesButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,5 +35,20 @@ class CreateRoutineVC: UIViewController, UICollectionViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: Functions
+    
+    @IBAction func nameRoutine(sender: UIButton) {
+        //user has clicked Done button after selecting their exercises
+        //show the hidden objects so the user can name their routine
+        nameYourRoutine.hidden = false
+        routineName.hidden = false
+        createButton.hidden = false
+        //hide the background items or fade them somehow
+        doneSelectingExercisesButton.hidden = true
+    }
+    
+    @IBAction func createRoutine(sender: UIButton) {
+        
+    }
     
 }
