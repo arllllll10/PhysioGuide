310aea25a8908dbb0b464d04e05dd7f21970566c
Rohm <rohm@d207-023-201-123.wireless.sfu.ca>
2015-12-05 19:12:39 -0800

    junk file!

---
+++ PhysioGuide_6.3_latest/Stats.swift
@@ -0,0 +1,17 @@
+//
+//  Stats.swift
+//  PhysioGuide_7
+//
+//  Created by Rohm on 12/5/15.
+//  Copyright © 2015 Team STAR. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class Stats{
+    // Lets handle stuff here!
+    
+    
+}
